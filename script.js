1b47d68348cff75e3e337ff57025da569f30aeb3
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-28 13:34:00 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+function getCharCode(params) {
+	let n=Math.random()*100;
+	if((48<=n&& n<=57)||(65<=n && n<90)||(97<=n&&n<99))
+	{
+		return n;
+	}
+	return parseInt(n/5)+100;
+	
+}
 function makeid(l) {
   // write your code here
+	let str="";
+	for(let i=1;i<=l;i++)
+		{
+			let charCode=getCharCode();
+			str+=String.fromCharCode(charCode)
+		}
+	return str;
 }
 
 // Do not change the code below.
